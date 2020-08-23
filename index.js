dbb353dbe295f02f2bbdac608573a5cc1512522a
CrunchyPancakes <30matthewt@gmail.com>
2020-08-23 14:40:55 +1200

    Fixed small error

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express(); // make our 'app' an express one.
 const port = process.env.PORT || 3000 // Port that the server will run on. Will use a default available one, otherwise 3000.
 
 app.use(express.static(__dirname + '/src')); //Add this line. Change public to whatever location your static assets are stored at
-app.get('/', (req, res) => res.sendFile(__dirname + '/html/frame0.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/src/html/frame0.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
